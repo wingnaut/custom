4d7e5fad85c4b3441d4c140fb1de04fa85ad5e57
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-02-25 14:55:14 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -46,7 +46,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: 'basicBot',
+        botName: 'drunkbØt',
         language: 'english',
         chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
         scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
