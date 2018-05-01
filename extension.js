96a3baa73c65be3daf597c9440b4a4b45892a027
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-05-01 12:27:54 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -213,6 +213,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/wingnaut/source/master/basicBot.js", extend);
 
 }).call(this);
