e8fa870565424db0a7fcd15cce515c875de030c0
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-05-01 19:40:33 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -110,7 +110,7 @@
         motdEnabled: false,
         motdInterval: 5,
         motd: 'Temporary Message of the Day',
-        filterChat: true,
+        filterChat: false,
         etaRestriction: false,
         welcome: true,
         opLink: null,
