a8c9c1358e469896c3ac8cdbbb79e9a1852ec015
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-05-01 21:21:55 -0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -38,8 +38,7 @@
           }
         };
 
-        //Props command1:
-
+        //Props command:
         bot.commands.propsCommand = {
             command: 'props',
             rank: 'user',
@@ -48,12 +47,15 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat(chat.un +" just gave props to @"+ API.getDJ().username +" for playing a great track!");
+                    API.sendChat(chat.un +" just gave props to @"+ API.getDJ().username +" for playing a rad track!");
                 }
             }
-        }
+        };
 
 
+        
+        
+        
         // Load the chat package again to account for any changes
         bot.loadChat();
 
