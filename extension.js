1dfe4e13d9431155804e63148fa24ea5a021bccf
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-08-11 11:38:45 -0700

    jif command

--- extension.js
+++ extension.js
@@ -46,7 +46,7 @@
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("http://i.giphy.com/eU60UxWVBMuEE.gif");
+              API.sendChat("http://media0.giphy.com/media/29yGKKh9FRrzi/giphy.gif");
             }
           }
         };        
