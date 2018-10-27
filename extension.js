93035a54378370168754ce478a6d1b34af0e2d4f
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-10-26 18:59:49 -0700

    added TED command

--- extension.js
+++ extension.js
@@ -110,6 +110,19 @@
           }
         };      
         
+        //  TED Command
+        bot.commands.tedCommand = {
+          command: 'ted',  
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("https://images.sex.com/images/pinporn/2017/07/06/620/18013139.gif");
+            }
+          }
+        };      
         
         // END STAFF COMMANDS
         
