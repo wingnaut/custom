9c595a984c551a0f3166f891d5dc4bf486108926
wingnaut <35912373+wingnaut@users.noreply.github.com>
2018-11-02 09:24:43 -0700

    added smith command

--- extension.js
+++ extension.js
@@ -96,6 +96,20 @@
           }
         }; 
         
+        // smith
+        bot.commands.smithCommand = {
+          command: 'smith',  
+          rank: 'user', // Minimum user permission to use the command
+          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+          functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+              API.sendChat("http://files.digitaltrends.com/images/canadian_club_pour.gif");
+            }
+          }
+        };
+        
         //  Sparky Command
         bot.commands.sparkyCommand = {
           command: 'sparky',  
